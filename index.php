946783ad1ea24ea97676a450e55cda8350ab7b66
iamajretuya-web <ajretuya.designs@gmail.com>
2026-06-10 10:03:04 +0800

    Added features for mobile app

--- index.php
+++ index.php
@@ -80,6 +80,12 @@ function getProgressStyle($consumedVal, $budgetVal) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Daily Macro Tracker</title>
     <link rel="stylesheet" href="style.css">
+
+    <link rel="manifest" href="manifest.json">
+    <meta name="theme-color" content="#F6F6F6">
+    <link rel="apple-touch-icon" href="logos/macros.png">
+
+
 </head>
 <body>
     <div class="container">
